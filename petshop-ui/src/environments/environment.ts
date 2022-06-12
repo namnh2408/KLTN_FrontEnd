53503c3705e6829c8a62a0be283c12550790074f
NghiaHuynh2000 <huynhtrongnghia1090@gmail.com>
2022-06-12 15:33:32 +0700

    update url

--- petshop-ui/src/environments/environment.ts
+++ petshop-ui/src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  urlImage: 'https://localhost:44312/ImageUI/',
-  apiUrl: 'https://localhost:44312/api/',
-  // urlImage: 'http://157.119.251.140:9713/ImageUI/',
-  // apiUrl: 'http://157.119.251.140:9713/api/',
+  // urlImage: 'https://localhost:44312/ImageUI/',
+  // apiUrl: 'https://localhost:44312/api/',
+  urlImage: 'http://157.119.251.140:9713/ImageUI/',
+  apiUrl: 'http://157.119.251.140:9713/api/',
   defaultToken: ''
 };
 
